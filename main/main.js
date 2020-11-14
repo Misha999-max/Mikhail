f5abd39899d75ac300e50c6a3595df31bcb0c120
Misha999-max <63150319+Misha999-max@users.noreply.github.com>
2020-11-14 16:19:18 +0300

    initial commit

---
+++ main/main.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#btn_click');
+const title = document.querySelector('h1');
+console.log(btn);
+btn.addEventListener('click',function(){
+ 
+    alert("first function");
+});
